0b041fee382b3c6d98971d6b25d7ec8380ea3e2c
Simonas Karuzas <simonas@not.cat>
2023-09-27 17:50:38 +0300

    feat: fetch by reference wip

--- src/MarkDown.tsx
+++ src/MarkDown.tsx
@@ -13,16 +13,35 @@ export const MarkDown: React.FC<{ content: string}> = ({ content }: { content: s
     return (<div>
       {parsed.map((token, index) => {
       if (token.type === 'fence' && token.info === 'vc+jwt') {
-        console.log('found vc jwt', token.content)
         const verifiableCredential = normalizeCredential(token.content.replace(/\s/g, ''))
         const hash = computeEntryHash(verifiableCredential)
         return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
       }
       if (token.type === 'fence' && token.info === 'vc+json') {
-        console.log('ahahahah')
         const verifiableCredential = JSON.parse(token.content)
         const hash = computeEntryHash(verifiableCredential)
         return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
+      } else if (token.type === 'fence' && token.info  === 'vc+multihash') {
+        const items = token.content.replace(/\s/g, '').split('/');
+        let hash = '';
+        let did = '';
+        if (items.length === 2) {
+          did = items[0];
+          hash = items[1];
+        } else {
+          hash = items[0];
+        }
+
+        // const verifiableCredential = await agent?.dataStoreGetVerifiableCredential({
+        //   hash,
+        // });
+
+        // if (!credential) {
+        //   // TODO: fetch using IPFS or DIDComm
+        // }
+
+        // return <VerifiableCredentialComponent credential={{hash, verifiableCredential}} />
+        return null
       }
       return <div dangerouslySetInnerHTML={{ __html: md.renderer.render([token], {}, {}) }} />
     })}</div>);
